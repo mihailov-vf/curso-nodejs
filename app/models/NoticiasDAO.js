86990b83942a1743b94ab93fc0fd20f717a26f6e
Mihailov Vasilievic Filho <mihailov@gmail.com>
2019-01-11 20:28:35 -0200

    Refatoração do model de notícias

---
+++ app/models/NoticiasDAO.js
@@ -0,0 +1,17 @@
+function NoticiasDAO(connection) {
+    this.connection = connection;
+}
+
+NoticiasDAO.prototype.getNoticias = function (callback) {
+    this.connection.query('SELECT * from noticias', callback);
+};
+NoticiasDAO.prototype.getNoticia = function (callback) {
+    this.connection.query('SELECT * from noticias WHERE id_noticia = 2', callback);
+};
+NoticiasDAO.prototype.salvarNoticia = function (noticia, callback) {
+    this.connection.query('INSERT INTO noticias set ?', noticia, callback);
+};
+
+module.exports = function () {
+    return NoticiasDAO;
+};
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -7,9 +7,9 @@ module.exports = function (app) {
         var noticia = req.body;
 
         var connection = app.config.dbConnection();
-        var noticiasModel = app.app.models.noticiasModel;
+        var noticiasDAO = new app.app.models.NoticiasDAO(connection);
 
-        noticiasModel.salvarNoticia(noticia, connection, function (error, result) {
+        noticiasDAO.salvarNoticia(noticia, function (error, result) {
             res.redirect('/noticias');
         });
     });

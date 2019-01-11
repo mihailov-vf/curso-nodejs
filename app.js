d1898590433fd8c528beef0d63b11542e3c2c5e5
Mihailov Vasilievic Filho <mihailov@gmail.com>
2019-01-11 10:09:53 -0200

    Aula 14 - utilizando ejs

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
+app.get('/tecnologia', function (req, res) {
+    res.render('secao/tecnologia');
+});
+
 app.get('/', function (req, res) {
     res.send('<html><body>Portal de notícias<body></html>');
-});
-app.get('/tecnologia', function (req, res) {
-    res.send('<html><body>Notícias de tecnologia<body></html>');
 });
 
 app.listen(3000, function () {

6fbb6626babc2bffdfafd03133cccf9a6490a9f8
Mihailov Vasilievic Filho <mihailov@gmail.com>
2019-01-11 13:47:43 -0200

    Refatoração - conexão de banco

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,11 +1,6 @@
+var connection = require('../../config/dbConnection')();
+
 module.exports = function (app) {
-    var mysql = require('mysql');
-    var connection = mysql.createConnection({
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'JackHammer',
-        database: 'portal_noticias'
-    });
 
     app.get('/noticias', function (req, res) {
         connection.query('Select * from noticias', function (error, result) {

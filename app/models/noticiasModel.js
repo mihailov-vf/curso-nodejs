75672ae60630a8320c5d29ae6dbe8ab844781c25
Mihailov Vasilievic Filho <mihailov@gmail.com>
2019-01-11 20:12:47 -0200

    Aula 39 - inserção de noticias

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -1,10 +1,14 @@
 module.exports = function () {
     this.getNoticias = function (connection, callback) {
-        connection.query('Select * from noticias', callback);
+        connection.query('SELECT * from noticias', callback);
     };
 
     this.getNoticia = function (connection, callback) {
-        connection.query('Select * from noticias WHERE id_noticia = 2', callback);
+        connection.query('SELECT * from noticias WHERE id_noticia = 2', callback);
+    };
+
+    this.salvarNoticia = function (noticia, connection, callback) {
+        connection.query('INSERT INTO noticias set ?', noticia, callback);
     };
 
     return this;

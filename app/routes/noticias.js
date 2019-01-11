d15c7499c8668d2d67f004efb22fd5527f63ae08
Mihailov Vasilievic Filho <mihailov@gmail.com>
2019-01-11 16:46:20 -0200

    Aula 34 - uso de model para noticias

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,8 +1,9 @@
 module.exports = function (app) {
     app.get('/noticias', function (req, res) {
         var connection = app.config.dbConnection();
+        var noticiasModel = app.app.models.noticiasModel;
 
-        connection.query('Select * from noticias', function (error, result) {
+        noticiasModel.getNoticias(connection, function (error, result) {
             res.render('noticias/noticias', {
                 noticias: result
             });
--- config/server.js
+++ config/server.js
@@ -7,8 +7,9 @@ app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 consign()
-    .include('./app/routes')
+    .include('app/routes')
     .then('config/dbConnection.js')
+    .then('app/models')
     .into(app);
 
 module.exports = app;

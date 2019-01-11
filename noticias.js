a2d9b42fe9c48ac7b21deaf77a08defad7de9fa4
Mihailov Vasilievic Filho <mihailov@gmail.com>
2019-01-11 09:41:20 -0200

    Aula 9

--- noticias.js
+++ noticias.js
@@ -1,7 +1,19 @@
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
-    res.end('<html><body>Notícias</body></html>');
+    var categoria = req.url;
+
+    if (categoria == '/tecnologia') {
+        res.end('<html><body>Notícias de tecnologia</body></html>');
+    }
+    if (categoria == '/moda') {
+        res.end('<html><body>Notícias de moda</body></html>');
+    }
+    if (categoria == '/beleza') {
+        res.end('<html><body>Notícias de beleza</body></html>');
+    }
+
+    res.end('<html><body>Portal de notícias</body></html>');
 });
 
 server.listen(3000);

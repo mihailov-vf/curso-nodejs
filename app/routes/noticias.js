57b62cbde66fcd63b6d4eb1f5d63643b8b67bb05
Mihailov Vasilievic Filho <mihailov@gmail.com>
2019-01-11 13:31:44 -0200

    Aula 24 - Utilizando MySQL

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,16 @@
 module.exports = function (app) {
+    var mysql = require('mysql');
+    var connection = mysql.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'JackHammer',
+        database: 'portal_noticias'
+    });
+
     app.get('/noticias', function (req, res) {
-        res.render('noticias/noticias');
+        connection.query('Select * from noticias', function (error, result) {
+            res.send(result);
+        });
+        //res.render('noticias/noticias');
     });
 };

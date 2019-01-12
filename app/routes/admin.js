a108c5391d80204f0bdb78f31ff0c0497f442c99
Mihailov Vasilievic Filho <mihailov@gmail.com>
2019-01-11 22:16:12 -0200

    Aula 44 - Validando campos de nova notícia

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -5,6 +5,20 @@ module.exports = function (app) {
 
     app.post('/noticias/salvar', function (req, res) {
         var noticia = req.body;
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+        req.assert('resumo', 'O resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+        req.assert('autor', 'Autor é obrigatório').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatório')
+            .notEmpty()
+            .isISO8601();
+        req.assert('noticia', 'Notícia é obrigatório').notEmpty();
+
+        var errors = req.validationErrors();
+        if (errors) {
+            res.render('admin/form_add_noticia');
+            return;
+        }
 
         var connection = app.config.dbConnection();
         var noticiasDAO = new app.app.models.NoticiasDAO(connection);
--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var validator = require('express-validator');
 
 var app = express();
 
@@ -10,6 +11,7 @@ app.set('views', './app/views');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(validator());
 
 consign()
     .include('app/routes')

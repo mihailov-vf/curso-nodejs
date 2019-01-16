440c9e22fe678cda1bf39c60a5ee4f0d71579598
Mihailov Vasilievic Filho <mihailov@gmail.com>
2019-01-16 17:51:31 -0200

    Views e rotas

---
+++ multiroom_chat/app/routes/chat.js
@@ -0,0 +1,8 @@
+module.exports = function (application) {
+    application.get('/chat', function (req, res) {
+        res.render('chat');
+    });
+    application.post('/chat', function (req, res) {
+        res.render('index');
+    });
+};

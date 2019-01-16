9dc4e91ef7d76da77636897263f4d471e39add43
Mihailov Vasilievic Filho <mihailov@gmail.com>
2019-01-16 17:56:03 -0200

    Controllers

--- multiroom_chat/app/routes/chat.js
+++ multiroom_chat/app/routes/chat.js
@@ -1,8 +1,8 @@
 module.exports = function (application) {
     application.get('/chat', function (req, res) {
-        res.render('chat');
+        application.app.controllers.chat.iniciaChat(application, req, res);
     });
     application.post('/chat', function (req, res) {
-        res.render('index');
+        application.app.controllers.chat.iniciaChat(application, req, res);
     });
 };

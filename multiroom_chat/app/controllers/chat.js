774b2087f7cb34d4d2da1af2118c6587241617c0
Mihailov Vasilievic Filho <mihailov@gmail.com>
2019-01-21 17:48:27 -0200

    Forme de entrada para o chat

--- multiroom_chat/app/controllers/chat.js
+++ multiroom_chat/app/controllers/chat.js
@@ -1,3 +1,14 @@
 module.exports.iniciaChat = function (application, req, res) {
+    var dadosForm = req.body;
+    req.assert('apelido', 'O Apelido é obrigatório').notEmpty();
+    req.assert('apelido', 'O Apelido deve ter entre 3 e 15 caracteres').len(3, 15);
+
+    var errors = req.validationErrors();
+    if (errors) {
+        res.render('index', {
+            errors: errors
+        });
+    }
+
     res.render('chat');
 };

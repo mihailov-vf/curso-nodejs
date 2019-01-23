237832ae9d160c40922a9fc10417f4fd2796addd
Mihailov Vasilievic Filho <mihailov@gmail.com>
2019-01-23 17:02:08 -0200

    Estrutura de cadastro Template, controller e tratamento de erros

--- mmorpg_got/app/controllers/cadastro.js
+++ mmorpg_got/app/controllers/cadastro.js
@@ -1,3 +1,27 @@
 module.exports.cadastro = function (application, req, res) {
-    res.render('cadastro');
+    res.render('cadastro', {
+        validacao: {},
+        dadosForm: {}
+    });
+};
+
+module.exports.cadastrar = function (application, req, res) {
+    var dadosForm = req.body;
+
+    req.assert('nome', 'Nome deve ser preenchido').notEmpty();
+    req.assert('usuario', 'Usuário deve ser preenchido').notEmpty();
+    req.assert('senha', 'Senha deve ser preenchido').notEmpty();
+    req.assert('casa', 'A Casa deve ser escolhida').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render('cadastro', {
+            validacao: errors,
+            dadosForm: dadosForm
+        });
+        return;
+    }
+
+    res.send('Podemos cadastrar');
 };

43a61b2924593f27e8ef5be546529614366f0670
Mihailov Vasilievic Filho <mihailov@gmail.com>
2019-01-24 15:22:02 -0200

    Autenticação e usuário para página de jogo

--- mmorpg_got/app/controllers/jogo.js
+++ mmorpg_got/app/controllers/jogo.js
@@ -1,3 +1,13 @@
 module.exports.jogo = function (application, req, res) {
+    if (!req.session.autorizado) {
+        res.redirect('/');
+    }
+
     res.render('jogo');
+};
+
+module.exports.sair = function (application, req, res) {
+    req.session.destroy(function (error) {
+        res.redirect('/');
+    });
 };

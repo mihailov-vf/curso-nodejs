5fa6aa314766f78f3d949ad81d4b5a65c9fb387b
Mihailov Vasilievic Filho <mihailov@gmail.com>
2019-01-24 13:44:53 -0200

    Formulário autenticação de usuário e validação de entradas

--- mmorpg_got/app/controllers/index.js
+++ mmorpg_got/app/controllers/index.js
@@ -1,3 +1,23 @@
 module.exports.index = function (application, req, res) {
-    res.render('index');
+    res.render('index', {
+        validacao: {}
+    });
+};
+
+module.exports.autenticar = function (application, req, res) {
+    var dadosForm = req.body;
+
+    req.assert('usuario', 'O campo "usuario" deve estar preenchido').notEmpty();
+    req.assert('senha', 'O campo "senha" deve estar preenchido').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.render('index', {
+            validacao: errors
+        });
+        return;
+    }
+
+    res.redirect('/jogo');
 };
